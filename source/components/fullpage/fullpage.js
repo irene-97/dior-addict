b8153b862f9d41f04de77ceb68714b059aca3fc6
Irene Artemova <artemova.irene@gmail.com>
2017-12-19 19:09:22 +0400

    Правки. Плавное пролистывание ко второму слайду

--- source/components/fullpage/fullpage.js
+++ source/components/fullpage/fullpage.js
@@ -4,6 +4,9 @@ $(document).ready(() => {
 	$('.fullpage').fullpage({
 		anchors: ['1', '2', '3', '4', '5', '6'],
 		menu: '.navigation',
+		onLeave(index, nextIndex) {
+			$('.navigation').toggleClass('navigation--visible', nextIndex !== 1);
+		},
 		afterLoad(anchorLink, index) {
 			setTimeout(() => {
 				$(`.navigation li:nth-child(${index})`).addClass('active-point').siblings().removeClass('active-point');
--- source/components/section-1/section-1.js
+++ source/components/section-1/section-1.js
@@ -1,1 +1,5 @@
 require('./_section-1.scss');
+
+$('.section--1 .section__arrow').on('click', () => {
+	$.fn.fullpage.moveSectionDown();
+});
