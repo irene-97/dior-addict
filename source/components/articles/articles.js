1d349457f740d64fc830461c4fb1fd761a641138
Irene Artemova <artemova.irene@gmail.com>
2018-01-09 10:34:19 +0400

    gh-pages

--- source/components/articles/articles.js
+++
@@ -1,5 +0,0 @@
-require('./_articles.scss');
-
-$('.articles__slider').slick({
-	speed: 1000,
-});
--- source/components/home/home.js
+++
@@ -1,5 +0,0 @@
-require('./_home.scss');
-
-$('.home__arrow').on('click', () => {
-	$.fn.fullpage.moveSectionDown();
-});
